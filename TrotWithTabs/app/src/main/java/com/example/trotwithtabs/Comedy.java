679ef46d4aff1e74fd12a2c20b15944ac3d04d89
asong57 <asong57@naver.com>
2021-01-12 21:50:35 +0900

    인기,예능 구성한 버전

--- TrotWithTabs/app/src/main/java/com/example/trotwithtabs/Comedy.java
+++ TrotWithTabs/app/src/main/java/com/example/trotwithtabs/Comedy.java
@@ -1,21 +1,232 @@
 package com.example.trotwithtabs;
 
-
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpRequestInitializer;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.youtube.YouTube;
+import com.google.api.services.youtube.model.ResourceId;
+import com.google.api.services.youtube.model.SearchListResponse;
+import com.google.api.services.youtube.model.SearchResult;
+import com.google.api.services.youtube.model.Thumbnail;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import nodeal.example.you_tube_recyclerview.YoutubeSinger;
+
 public class Comedy extends Fragment {
+
+    MainActivity activity;
+    Context context;
+
+    private static final String TAG = "singer";
+
+    private String API_KEY = "AIzaSyCYCo80nxyEgApqfVmfilFC04T-rXWBRBI";
+    private String result;
+    int singerPosition;
+    String[] list_singer = {"뽕숭아학당","사랑의 콜센타","트롯 전국체전","트로트의 민족"};
+    ArrayList<SingerInfoList> singerInfoList;
+    ArrayList<SingerInfoList> singerInfoList2 = new ArrayList<>();
+
+
+
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        this.context = context;
+
+        activity = (MainActivity) getActivity();
+
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.comedy, container, false);
+        ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.singer, container, false);
+
+        ListView listView = (ListView) rootView.findViewById(R.id.listViewSinger);
+
+        SingerAdapter adapter = new SingerAdapter();
+        adapter.addItem(new SingerItem("뽕숭아학당"));
+        adapter.addItem(new SingerItem("사랑의 콜센타"));
+        adapter.addItem(new SingerItem("트롯 전국체전"));
+        adapter.addItem(new SingerItem("트로트의 민족"));
+
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+
+                YoutubeAsyncTask youtubeAsyncTask = new YoutubeAsyncTask();
+                youtubeAsyncTask.execute();
+                singerPosition= ReturnSingerPosition(position);
+
+            }
+        });
 
         return rootView;
     }
+
+    public int ReturnSingerPosition(int position){
+        return position;
+    }
+
+    private class YoutubeAsyncTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try {
+                HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
+                final JsonFactory JSON_FACTORY = new JacksonFactory();
+                final long NUMBER_OF_VIDEOS_RETURNED = 15;
+
+                YouTube youtube = new YouTube.Builder(HTTP_TRANSPORT, JSON_FACTORY, new HttpRequestInitializer() {
+                    public void initialize(HttpRequest request) throws IOException {
+                    }
+                }).setApplicationName("youtube-search-sample").build();
+
+                YouTube.Search.List search = youtube.search().list("id,snippet");
+
+                search.setKey(API_KEY);
+
+                String singerName=list_singer[singerPosition];
+                search.setQ(singerName);
+                // search.setChannelId("UCk9GmdlDTBfgGRb7vXeRMoQ"); //레드벨벳 공식 유투브 채널
+                search.setOrder("relevance"); //date relevance
+
+                search.setType("video");
+
+                search.setFields("items(id/kind,id/videoId,snippet/title,snippet/thumbnails/default/url)");
+                search.setMaxResults(NUMBER_OF_VIDEOS_RETURNED);
+                SearchListResponse searchResponse = search.execute();
+
+                List<SearchResult> searchResultList = searchResponse.getItems();
+
+                if (searchResultList != null) {
+                    prettyPrint(searchResultList.iterator(), singerName);
+                }
+            } catch (GoogleJsonResponseException e) {
+                System.err.println("There was a service error: " + e.getDetails().getCode() + " : "
+                        + e.getDetails().getMessage());
+                System.err.println("There was a service error 2: " + e.getLocalizedMessage() + " , " + e.toString());
+            } catch (IOException e) {
+                System.err.println("There was an IO error: " + e.getCause() + " : " + e.getMessage());
+            } catch (Throwable t) {
+                t.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            Fragment comedyDetailFragment = new ComedyDetail();
+            singerInfoList=singerInfoList2;
+
+            Bundle bundle=new Bundle();
+            bundle.putParcelableArrayList("singerInfoList",(ArrayList<? extends Parcelable>) singerInfoList);
+            comedyDetailFragment.setArguments(bundle);
+
+            ((MainActivity)getActivity()).getSupportFragmentManager().beginTransaction().replace(R.id.container, comedyDetailFragment).commit();
+        }
+
+        public void prettyPrint(Iterator<SearchResult> iteratorSearchResults, String query) {
+            if (!iteratorSearchResults.hasNext()) {
+                System.out.println(" There aren't any results for your query.");
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            while (iteratorSearchResults.hasNext()) {
+                SearchResult singleVideo = iteratorSearchResults.next();
+                ResourceId rId = singleVideo.getId();
+
+                // Double checks the kind is video.
+                if (rId.getKind().equals("youtube#video")) {
+
+                    Thumbnail thumbnail = (Thumbnail) singleVideo.getSnippet().getThumbnails().get("default");
+                    singerInfoList2.add(new SingerInfoList(singleVideo.getSnippet().getTitle(),rId.getVideoId(),thumbnail.getUrl()));
+                    Log.d(TAG,singerInfoList2.get(0).title);
+                    Log.d(TAG,"하긴 함");
+                }
+
+            }
+
+            result = sb.toString();
+        }
+    }
+
+
+    class SingerAdapter extends BaseAdapter {
+        ArrayList<SingerItem> items = new ArrayList<SingerItem>();
+
+        @Override
+        public int getCount() {
+            return items.size();
+        }
+
+        public void addItem(SingerItem item){
+            items.add(item);
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return items.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            GenreItemView view = new GenreItemView(getContext());
+
+            SingerItem item = items.get(position);
+            view.setName(item.getName());
+            return view;
+        }
+
+        private void getApplicationContext() {
+        }
+    }
+
 }
--- TrotWithTabs/app/src/main/java/com/example/trotwithtabs/MainActivity.java
+++ TrotWithTabs/app/src/main/java/com/example/trotwithtabs/MainActivity.java
@@ -1,22 +1,45 @@
 package com.example.trotwithtabs;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.ListFragment;
 
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
 
 import com.google.android.material.tabs.TabLayout;
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpRequestInitializer;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.youtube.YouTube;
+import com.google.api.services.youtube.model.ResourceId;
+import com.google.api.services.youtube.model.SearchListResponse;
+import com.google.api.services.youtube.model.SearchResult;
+import com.google.api.services.youtube.model.Thumbnail;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     Home fragment0;
@@ -39,7 +62,6 @@ public class MainActivity extends AppCompatActivity {
 
         fragment0 = new Home();
         fragment1 = new Genre();
-        fragment2 = new Popular();
         fragment3 = new Singer();
         fragment4 = new Comedy();
         fragment5 = new Jjim();
@@ -62,19 +84,25 @@ public class MainActivity extends AppCompatActivity {
                 Fragment selected = null;
                 if(position == 0) {
                     selected = fragment0;
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, selected).commit();
                 }else if(position==1){
                     selected = fragment1;
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, selected).commit();
                 }else if(position==2){
-                    selected = fragment2;
+                    YoutubeAsyncTask youtubeAsyncTask = new YoutubeAsyncTask();
+                    youtubeAsyncTask.execute();
                 }else if(position==3){
                     selected = fragment3;
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, selected).commit();
                 }else if(position==4){
                     selected = fragment4;
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, selected).commit();
                 }else if(position==5){
                     selected = fragment5;
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, selected).commit();
                 }
 
-                getSupportFragmentManager().beginTransaction().replace(R.id.container, selected).commit();
+
 
             }
 
@@ -93,5 +121,143 @@ public class MainActivity extends AppCompatActivity {
     public void fragBtnClick(Bundle bundle) {
         this.mBundle = bundle;
     } //fragBtnClcick()
+
+    MainActivity activity;
+    Context context;
+
+    private static final String TAG = "singer";
+
+    private String API_KEY = "AIzaSyCYCo80nxyEgApqfVmfilFC04T-rXWBRBI";
+    private String result;
+
+    ArrayList<SingerInfoList> singerInfoList;
+    ArrayList<SingerInfoList> singerInfoList2 = new ArrayList<>();
+
+
+    private class YoutubeAsyncTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try {
+                HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
+                final JsonFactory JSON_FACTORY = new JacksonFactory();
+                final long NUMBER_OF_VIDEOS_RETURNED = 15;
+
+                YouTube youtube = new YouTube.Builder(HTTP_TRANSPORT, JSON_FACTORY, new HttpRequestInitializer() {
+                    public void initialize(HttpRequest request) throws IOException {
+                    }
+                }).setApplicationName("youtube-search-sample").build();
+
+                YouTube.Search.List search = youtube.search().list("id,snippet");
+
+                search.setKey(API_KEY);
+
+                search.setQ("트로트");
+                // search.setChannelId("UCk9GmdlDTBfgGRb7vXeRMoQ"); //레드벨벳 공식 유투브 채널
+                search.setOrder("relevance"); //date relevance
+
+                search.setType("video");
+
+                search.setFields("items(id/kind,id/videoId,snippet/title,snippet/thumbnails/default/url)");
+                search.setMaxResults(NUMBER_OF_VIDEOS_RETURNED);
+                SearchListResponse searchResponse = search.execute();
+
+                List<SearchResult> searchResultList = searchResponse.getItems();
+
+                if (searchResultList != null) {
+                    prettyPrint(searchResultList.iterator(), "트로트");
+                }
+            } catch (GoogleJsonResponseException e) {
+                System.err.println("There was a service error: " + e.getDetails().getCode() + " : "
+                        + e.getDetails().getMessage());
+                System.err.println("There was a service error 2: " + e.getLocalizedMessage() + " , " + e.toString());
+            } catch (IOException e) {
+                System.err.println("There was an IO error: " + e.getCause() + " : " + e.getMessage());
+            } catch (Throwable t) {
+                t.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            fragment2 = new Popular();
+            singerInfoList=singerInfoList2;
+
+            Bundle bundle=new Bundle();
+            bundle.putParcelableArrayList("singerInfoList",(ArrayList<? extends Parcelable>) singerInfoList);
+            fragment2.setArguments(bundle);
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment2).commit();
+        }
+
+        public void prettyPrint(Iterator<SearchResult> iteratorSearchResults, String query) {
+            if (!iteratorSearchResults.hasNext()) {
+                System.out.println(" There aren't any results for your query.");
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            while (iteratorSearchResults.hasNext()) {
+                SearchResult singleVideo = iteratorSearchResults.next();
+                ResourceId rId = singleVideo.getId();
+
+                // Double checks the kind is video.
+                if (rId.getKind().equals("youtube#video")) {
+
+                    Thumbnail thumbnail = (Thumbnail) singleVideo.getSnippet().getThumbnails().get("default");
+                    singerInfoList2.add(new SingerInfoList(singleVideo.getSnippet().getTitle(),rId.getVideoId(),thumbnail.getUrl()));
+                    Log.d(TAG,singerInfoList2.get(0).title);
+                    Log.d(TAG,"하긴 함");
+                }
+
+            }
+
+            result = sb.toString();
+        }
+    }
+
+
+    class SingerAdapter extends BaseAdapter {
+        ArrayList<SingerItem> items = new ArrayList<SingerItem>();
+
+        @Override
+        public int getCount() {
+            return items.size();
+        }
+
+        public void addItem(SingerItem item){
+            items.add(item);
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return items.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            SingerItemView view = new SingerItemView(MainActivity.this.getApplicationContext());
+
+            SingerItem item = items.get(position);
+            view.setName(item.getName());
+            return view;
+        }
+
+        private void getApplicationContext() {
+        }
+    }
+
 
 }
