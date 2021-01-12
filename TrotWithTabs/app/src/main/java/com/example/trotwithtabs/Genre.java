ecf90d748868eccf99b6e23edfea1841319761eb
GlassK <glassk0724@gmail.com>
2021-01-12 22:44:44 +0900

    Add: genre

--- TrotWithTabs/app/src/main/java/com/example/trotwithtabs/Genre.java
+++ TrotWithTabs/app/src/main/java/com/example/trotwithtabs/Genre.java
@@ -1,9 +1,15 @@
 package com.example.trotwithtabs;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
 
@@ -11,14 +17,51 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpRequestInitializer;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.youtube.YouTube;
+import com.google.api.services.youtube.model.ResourceId;
+import com.google.api.services.youtube.model.SearchListResponse;
+import com.google.api.services.youtube.model.SearchResult;
+import com.google.api.services.youtube.model.Thumbnail;
+
+import java.io.IOException;
+import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class Genre extends Fragment {
+    MainActivity activity;
+    Context context;
+
+    private static final String TAG = "genre";
+
+    private String API_KEY = "AIzaSyCYCo80nxyEgApqfVmfilFC04T-rXWBRBI";
+    private String result;
+    int genrePosition;
+    String[] list_genre = {"트로트 메들리","댄스트로트","전설의 트로트","최신 트로트","보이스트롯","미스트롯 송가인","미스터트롯","7080 메들리","7080 인기가수","강변가요제","대학가요제"};
+    ArrayList<GenreInfoList> genreInfoList;
+    ArrayList<GenreInfoList> genreInfoList2 = new ArrayList<>();
+
+
+
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        this.context = context;
+
+        activity = (MainActivity) getActivity();
+
+    }
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
     }
 
     @Nullable
@@ -28,21 +71,133 @@ public class Genre extends Fragment {
 
         ListView listView = (ListView) rootView.findViewById(R.id.listViewGenre);
 
-       GenreAdapter adapter = new GenreAdapter();
+        GenreAdapter adapter = new GenreAdapter();
         adapter.addItem(new GenreItem("트로트 메들리"));
-        adapter.addItem(new GenreItem("댄스 트로트"));
+        adapter.addItem(new GenreItem("댄스트로트"));
         adapter.addItem(new GenreItem("전설의 트로트"));
-        adapter.addItem(new GenreItem("최신트로트"));
+        adapter.addItem(new GenreItem("최신 트로트"));
         adapter.addItem(new GenreItem("보이스트롯"));
         adapter.addItem(new GenreItem("미스트롯 송가인"));
         adapter.addItem(new GenreItem("미스터트롯"));
         adapter.addItem(new GenreItem("7080 메들리"));
         adapter.addItem(new GenreItem("7080 인기가수"));
+        adapter.addItem(new GenreItem("강변가요제"));
+        adapter.addItem(new GenreItem("대학가요제"));
 
         listView.setAdapter(adapter);
 
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+
+                YoutubeAsyncTask youtubeAsyncTask = new YoutubeAsyncTask();
+                youtubeAsyncTask.execute();
+                genrePosition= ReturnGenrePosition(position);
+
+            }
+        });
+
         return rootView;
     }
+
+    public int ReturnGenrePosition(int position){
+        return position;
+    }
+
+    private class YoutubeAsyncTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try {
+                HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
+                final JsonFactory JSON_FACTORY = new JacksonFactory();
+                final long NUMBER_OF_VIDEOS_RETURNED = 15;
+
+                YouTube youtube = new YouTube.Builder(HTTP_TRANSPORT, JSON_FACTORY, new HttpRequestInitializer() {
+                    public void initialize(HttpRequest request) throws IOException {
+                    }
+                }).setApplicationName("youtube-search-sample").build();
+
+                YouTube.Search.List search = youtube.search().list("id,snippet");
+
+                search.setKey(API_KEY);
+
+                String genreName=list_genre[genrePosition];
+                search.setQ(genreName);
+                // search.setChannelId("UCk9GmdlDTBfgGRb7vXeRMoQ"); //레드벨벳 공식 유투브 채널
+                search.setOrder("relevance"); //date relevance
+
+                search.setType("video");
+
+                search.setFields("items(id/kind,id/videoId,snippet/title,snippet/thumbnails/default/url)");
+                search.setMaxResults(NUMBER_OF_VIDEOS_RETURNED);
+                SearchListResponse searchResponse = search.execute();
+
+                List<SearchResult> searchResultList = searchResponse.getItems();
+
+                if (searchResultList != null) {
+                    prettyPrint(searchResultList.iterator(), genreName);
+                }
+            } catch (GoogleJsonResponseException e) {
+                System.err.println("There was a service error: " + e.getDetails().getCode() + " : "
+                        + e.getDetails().getMessage());
+                System.err.println("There was a service error 2: " + e.getLocalizedMessage() + " , " + e.toString());
+            } catch (IOException e) {
+                System.err.println("There was an IO error: " + e.getCause() + " : " + e.getMessage());
+            } catch (Throwable t) {
+                t.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            Fragment genreDetailFragment = new GenreDetail();
+            genreInfoList=genreInfoList2;
+
+            Bundle bundle=new Bundle();
+            bundle.putParcelableArrayList("genreInfoList",(ArrayList<? extends Parcelable>) genreInfoList);
+            genreDetailFragment.setArguments(bundle);
+
+            ((MainActivity)getActivity()).getSupportFragmentManager().beginTransaction().replace(R.id.container, genreDetailFragment).commit();
+        }
+
+        public void prettyPrint(Iterator<SearchResult> iteratorSearchResults, String query) {
+            if (!iteratorSearchResults.hasNext()) {
+                System.out.println(" There aren't any results for your query.");
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            while (iteratorSearchResults.hasNext()) {
+                SearchResult singleVideo = iteratorSearchResults.next();
+                ResourceId rId = singleVideo.getId();
+
+                // Double checks the kind is video.
+                if (rId.getKind().equals("youtube#video")) {
+
+                    Thumbnail thumbnail = (Thumbnail) singleVideo.getSnippet().getThumbnails().get("default");
+                    genreInfoList2.add(new GenreInfoList(singleVideo.getSnippet().getTitle(),rId.getVideoId(),thumbnail.getUrl()));
+                    Log.d(TAG,genreInfoList2.get(0).title);
+                    Log.d(TAG,"하긴 함");
+                }
+
+            }
+
+            result = sb.toString();
+        }
+    }
+
+
     class GenreAdapter extends BaseAdapter {
         ArrayList<GenreItem> items = new ArrayList<GenreItem>();
 
@@ -51,7 +206,7 @@ public class Genre extends Fragment {
             return items.size();
         }
 
-        public void addItem(GenreItem item) {
+        public void addItem(GenreItem item){
             items.add(item);
         }
 
@@ -65,22 +220,16 @@ public class Genre extends Fragment {
             return position;
         }
 
-        //실무에서는 아래처럼 쓰면 부족하다
-        //만약 천 개의 아이템이 있을 때 모든 아이템이 다 로딩되면 메모리가 엄청 많이 쓰일 것
-        //마찬가지로 뷰 객체가 너무 많이 만들어지면 메모리를 엄청 많이 소모할 수 있다 - 스크롤할 때 리스트뷰가 끊어지는 느낌이 남
-        //그래서 화면에 보이지 않는 것들은 재사용하게 하여(convertView) new를 만들 필요가 없게 해주면 된다
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            GenreItemView view = null;
-            if (convertView == null) {
-                view = new GenreItemView(getContext());
-            } else {
-                view = (GenreItemView) convertView;
-            }
+            GenreItemView view = new GenreItemView(getContext());
 
             GenreItem item = items.get(position);
             view.setName(item.getName());
             return view;
+        }
+
+        private void getApplicationContext() {
         }
     }
 
